78ea5297166a21687d9d63ab364ff8c5a223fd05
Youngmi Kim <72649415+kym9129@users.noreply.github.com>
2023-07-02 19:54:15 +0900

    [unrated] Title: 소문자로 바꾸기, Time: 0.56 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public string solution(string myString) {
+        string answer = "";
+        answer = myString.ToLower();
+        
+        return answer;
+    }
+}
