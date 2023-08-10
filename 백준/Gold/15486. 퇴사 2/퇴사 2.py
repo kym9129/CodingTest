92a0e28527c714017176fd5fafbbe8dbb4512520
Youngmi Kim <72649415+kym9129@users.noreply.github.com>
2023-08-10 19:31:02 +0900

    [Gold V] Title: 퇴사 2, Time: 2304 ms, Memory: 251328 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,25 +1,23 @@
 import sys
+# sys.stdin = open("./python/backjoon/backjoonInput.txt", "rt") # vscode
 
-# 참고 : https://great-park.tistory.com/48
-
+# n = int(input())
 n = int(sys.stdin.readline())
-consulting_list = [(0, 0)] * (n+1)
-dp = [0] * (n+2)
-
-for i in range(1, n+1):
+consulting_list = list()
+for _ in range(n):
+    # time, price = map(int, input().split())
     time, price = map(int, sys.stdin.readline().split())
-    consulting_list[i] = (time, price)
+    consulting_list.append((time, price))
 
-leave_day = n+1
+LEAVE_DAY = n+1
+dp = [0] * (n+1)
 
-for i in range(n, -1, -1): # n~0까지 역순
+for i in range(n-1, -1, -1):
     time, price = consulting_list[i]
-
-    if i + time > leave_day:
-        dp[i] = dp[i+1] # 여기 때문에 dp 선언 시 n+2개로 선언
-    
+    if i + time >= LEAVE_DAY:
+        dp[i] = dp[i+1]
     else:
-        # i일에 상담하는 것과 상담 안하는 것 중 큰 것을 선택
-        dp[i] = max(dp[i+time] + price , dp[i+1])
-
+        dp[i] = max(dp[i+time]+price, dp[i+1])
+    
+# print(dp)
 print(dp[0])
