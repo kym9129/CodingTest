7796053fa787180e7f7781f09d33b85dcc0c43b5
Youngmi Kim <72649415+kym9129@users.noreply.github.com>
2024-06-14 00:31:15 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 2.57 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,25 +1,17 @@
 def solution(friends, gifts):
-    # 주고받은 테이블
+     # 주고받은 테이블
     gift_table = [[0 for _ in range(len(friends))] for _ in range(len(friends))]
+    # 지수
+    idxs = [0 for _ in range(len(friends))]
     for gift_log in gifts:
         sender, reciever = gift_log.split()
 
-        gift_table[friends.index(sender)][friends.index(reciever)] += 1
-    
-    # print(gift_table)
+        x = friends.index(sender)
+        y = friends.index(reciever)
 
-    # 지수
-    idxs = [0 for _ in range(len(friends))]
-    for i in range(len(friends)):
-
-        send_sum = 0
-        recieve_sum = 0
-        for j in range(len(friends)):
-            send_sum += gift_table[i][j]
-            recieve_sum += gift_table[j][i]
-        
-        idxs[i] = send_sum - recieve_sum
-
+        gift_table[x][y] += 1
+        idxs[x] += 1
+        idxs[y] -= 1
 
     # 비교
     rank = [0 for _ in range(len(friends))]
@@ -32,22 +24,8 @@ def solution(friends, gifts):
                 # 지수 비교
                 if idxs[a] > idxs[b]:
                     rank[a] += 1
-                elif idxs[b] > idxs[a]:
-                    rank[b] += 1
             
             elif gift_table[a][b] > gift_table[b][a]:
                 rank[a] += 1
-            elif gift_table[b][a] > gift_table[a][b]:
-                rank[b] += 1
 
-    rank2 = list()
-    for x in rank:
-        if x == 0:
-            rank2.append(x)
-            continue
-
-        rank2.append(x // 2)
-
-    rank2.sort(reverse=True)
-
-    return rank2[0]
+    return max(rank)
